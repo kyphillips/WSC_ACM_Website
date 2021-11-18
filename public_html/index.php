9c0fe0cc454c50fb34102c59b57e0576c77ff3d0
brsmit1993 <91701876+brsmit1993@users.noreply.github.com>
2021-11-18 11:29:01 -0600

    Init chagnes for index.php as well as additional file structure (#10)
    Co-authored-by: Brandon Smith <brsmit09@wsc.edu>

---
+++ public_html/index.php
@@ -0,0 +1,165 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>ACM Landing Page</title>
+
+		<link rel="stylesheet" href="CSS/main.css">
+		<link rel="stylesheet" href="CSS/LandingPage.css">
+		 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+		<!-- Popper JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+	</head>
+
+	<body>
+		<!-- START OF VIDEO LANDING PAGE -->
+		<!-- START OF VIDEO LANDING PAGE -->
+		<!-- START OF VIDEO LANDING PAGE -->
+			<div class="showcase">
+			
+				<div class="video-container">
+				<video src="Images/Landingpage/ACM_Landing.mp4" autoplay muted loop></video>
+				</div>
+
+				<div class="LpNavbarDiv">
+					<?php
+						include 'LandingpageNavbar.php';
+					?>
+				</div>
+				
+				<div>
+					<div class="acmLandingContent">
+						<div class="acmLandingImgDiv"><img class = "acmLandingImg" src = "Images/Landingpage/ACM_LandingLogo.png"></div>
+						<div class="LadningText01">Wayne State College</div>
+						<div class="LadningText02">Where All WSC students are welcome!</div>
+					</div>
+				</div>
+				
+				<div class="acmLandingContentBtm">
+					<div class="LadningText02">Why Join ACM?</div>
+					<div class="downArrowImgDiv animated infinite bounce"><img src ="Images/Landingpage/DownArrow.png"></div>
+				</div>
+
+			</div>
+		<!-- END OF VIDEO LANDING PAGE -->
+		<!-- END OF VIDEO LANDING PAGE -->
+		<!-- END OF VIDEO LANDING PAGE -->
+		<div class="row no-gutters">
+			<div class="col-lg-1"></div>
+
+			<div class="col-lg-10">
+				
+				<div class = "headerContainer01">
+					<span class ="header01">Not Just A Computer Club</span>
+				</div>
+
+				<div class= "row no-gutters">
+					<div class="col-lg-4 ">
+
+						<div class="header02">
+							<img class ="img01" src ="Images/Landingpage/ClubFair.jpg">
+							Meet New People
+						</div>
+
+						
+						<div class="pContent01">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel risus massa. Nulla efficitur lorem at ipsum condimentum interdum. 
+							Sed iaculis nisi non laoreet faucibus. Pellentesque scelerisque imperdiet condimentum. Quisque et efficitur mauris. Aenean volutpat dictum justo eu iaculis. 
+						</div>
+						
+
+					</div>
+					<div class="col-lg-4">
+
+						<div class="header02">
+							<img class ="img01" src ="Images/Landingpage/WebTeam.jpg">
+							Make New Friends
+						</div>
+
+						<div class="pContent01">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel risus massa. Nulla efficitur lorem at ipsum condimentum interdum. 
+							Sed iaculis nisi non laoreet faucibus. Pellentesque scelerisque imperdiet condimentum. Quisque et efficitur mauris. Aenean volutpat dictum justo eu iaculis. 
+						</div>
+					</div>
+					<div class="col-lg-4">
+						
+						<div class="header02">
+							<img class ="img01" src ="Images/Landingpage/BowlingNight.jpg">
+							Be Social
+						</div>
+
+						
+						<div class="pContent01">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel risus massa. Nulla efficitur lorem at ipsum condimentum interdum. 
+							Sed iaculis nisi non laoreet faucibus. Pellentesque scelerisque imperdiet condimentum. Quisque et efficitur mauris. Aenean volutpat dictum justo eu iaculis. 
+						</div>
+
+					</div>
+				</div>
+
+				<div class = "headerContainer02">
+					<span class ="header01">But Wait There's More</span>
+				</div>
+
+				<div class= "row no-gutters">
+					<div class="col-lg-6">
+						<div class="header02">
+							<img class ="img02" src ="Images/Landingpage/WebTeam.jpg">
+							
+						</div>
+					</div>
+					<div class="col-lg-6">
+					<div class="pContent01">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel risus massa. Nulla efficitur lorem at ipsum condimentum interdum. 
+							Sed iaculis nisi non laoreet faucibus. Pellentesque scelerisque imperdiet condimentum. Quisque et efficitur mauris. Aenean volutpat dictum justo eu iaculis. 
+						</div>
+					</div>
+
+						<div class="col-lg-6">
+					<div class="pContent01">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel risus massa. Nulla efficitur lorem at ipsum condimentum interdum. 
+							Sed iaculis nisi non laoreet faucibus. Pellentesque scelerisque imperdiet condimentum. Quisque et efficitur mauris. Aenean volutpat dictum justo eu iaculis. 
+						</div>
+						</div>
+
+					<div class="col-lg-6">
+						
+						<div class="header02">
+							<img class ="img02" src ="Images/Landingpage/BowlingNight.jpg">
+							
+						</div>
+
+					</div>
+					<div class="col-lg-6">
+						<div class="header02">
+							<img class ="img02" src ="Images/Landingpage/WebTeam.jpg">
+							
+						</div>
+					</div>
+					<div class="col-lg-6">
+					<div class="pContent01">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel risus massa. Nulla efficitur lorem at ipsum condimentum interdum. 
+							Sed iaculis nisi non laoreet faucibus. Pellentesque scelerisque imperdiet condimentum. Quisque et efficitur mauris. Aenean volutpat dictum justo eu iaculis. 
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+
+			<div class="col-lg-1"></div>
+		</div>
+	</body>
+
+</html>
