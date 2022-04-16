f776f7958949c5aacd6fc8c492684a59617ad983
brsmit1993 <91701876+brsmit1993@users.noreply.github.com>
2022-04-16 13:41:40 -0500

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -2,20 +2,16 @@
 <html>
 
 	<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
         <meta http-equiv="Pragma" content="no-cache" />
         <meta http-equiv="Expires" content="0" />
 
-		<title>WSC/ACM Landing Page</title>
+        <title>WSC/ACM - About Us</title>
 
-		<link rel="stylesheet" href="Resources/Landingpage/CSS/LandingVideo/LandingVideo.css">
-
-		 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
-
-
-
+        <link rel="stylesheet" href="CSS/Shared.css" />
+        <link rel="stylesheet" href="CSS/Index.css" />
 
 		<!-- Bootstrap CSS -->
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
